9d202be1efc22aaa813ca1dc5477e5d431053612
fin10 <fender64@gmail.com>
2020-12-13 00:46:50 +0900

    Replace static middleware instead of parcel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientMiddleware } from './middleware/client.middleware';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'client'),
+      exclude: ['/api*'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): void {
-    consumer.apply(ClientMiddleware).forRoutes({ path: '*', method: RequestMethod.GET });
-  }
-}
+export class AppModule {}
