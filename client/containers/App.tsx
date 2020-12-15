fb3ad6164e24676f59e39223bd01fd1ef93e827f
fin10 <fender64@gmail.com>
2020-12-15 17:28:19 +0900

    Add router

--- client/containers/App.tsx
+++ client/containers/App.tsx
@@ -1,47 +1,17 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
-import { List, ListItem, ListItemText } from '@material-ui/core';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NoteList from './NoteList';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: '100%',
-      maxWidth: 360,
-      backgroundColor: theme.palette.background.paper,
-    },
-  }),
-);
-
-const fetchNotes = async (update: React.Dispatch<React.SetStateAction<Note[]>>) => {
-  try {
-    const res = await axios.get<Note[]>('/api/notes');
-    update(res.data);
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-export default (): React.ReactElement => {
-  const [notes, updateNotes] = useState<Note[]>([]);
-  const classes = useStyles();
-
-  useEffect(() => {
-    fetchNotes(updateNotes);
-  }, []);
-
+const App = (): React.ReactElement => {
   return (
-    <div className={classes.root}>
-      <h2>Queueing</h2>
-      <List>
-        {notes.map((note) => {
-          return (
-            <ListItem key={note._id}>
-              <ListItemText primary={note.title} />
-            </ListItem>
-          );
-        })}
-      </List>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <NoteList />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
+
+export default App;
---
+++ client/containers/NoteList.tsx
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import { List, ListItem, ListItemText, Fab } from '@material-ui/core';
+import { Create as CreateIcon } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+    },
+    fab: {
+      position: 'absolute',
+      right: 16,
+      bottom: 16,
+    },
+  }),
+);
+
+const fetchNotes = async (update: React.Dispatch<React.SetStateAction<Note[]>>) => {
+  try {
+    const res = await axios.get<Note[]>('/api/notes');
+    update(res.data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const NoteList = (): React.ReactElement => {
+  const [notes, updateNotes] = useState<Note[]>([]);
+  const classes = useStyles();
+
+  useEffect(() => {
+    fetchNotes(updateNotes);
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      <h2>Queueing</h2>
+      <List>
+        {notes.map((note) => {
+          return (
+            <ListItem key={note._id}>
+              <ListItemText primary={note.title} />
+            </ListItem>
+          );
+        })}
+      </List>
+      <Fab className={classes.fab} color="primary" aria-label="create">
+        <CreateIcon />
+      </Fab>
+    </div>
+  );
+};
+
+export default NoteList;
