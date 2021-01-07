171a3238aedde02537637101dbe5cf00d71cac4f
fin10 <fender64@gmail.com>
2021-01-07 22:59:52 +0900

    Add tc for resources

---
+++ client/resources/Resources.spec.ts
@@ -0,0 +1,11 @@
+import { Resources } from './Resources';
+import { StringID } from './StringID';
+
+describe('Resources', () => {
+  it('string resources should be defined for all of string IDs', () => {
+    for (const id in StringID) {
+      const string = Resources.getString(id as StringID);
+      expect(string).not.toEqual('UNDEFINED');
+    }
+  });
+});
