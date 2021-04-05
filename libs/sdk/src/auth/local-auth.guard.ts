0fff4409b5253cdf336d3c36f6e4fcc8fa3413d8
fin10 <fender64@gmail.com>
2021-04-06 00:40:21 +0900

    Fix not login with local

--- libs/sdk/src/auth/local-auth.guard.ts
+++ libs/sdk/src/auth/local-auth.guard.ts
@@ -1,5 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, Logger } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  private readonly logger = new Logger(LocalAuthGuard.name);
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    if (request.isAuthenticated()) return true;
+
+    try {
+      const can = await super.canActivate(context);
+      if (can) {
+        await super.logIn(request);
+        return true;
+      }
+    } catch (err) {
+      this.logger.error('Failed to login with local.', err.stack);
+    }
+
+    return false;
+  }
+}
