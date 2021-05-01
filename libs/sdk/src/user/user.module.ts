8749dc9c3b3ede4acec2beaea0350f9ce57c08bc
fin10 <fender64@gmail.com>
2021-05-02 08:40:19 +0900

    Add user updated event

--- libs/sdk/src/user/user.module.ts
+++ libs/sdk/src/user/user.module.ts
@@ -1,12 +1,32 @@
 import { Module } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserUpdatedEvent } from './events/user-updated.event';
 import { PenaltyService } from './penalty.service';
 import { User, UserSchema } from './schemas/user.schema';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema, collection: 'users' }])],
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        imports: [EventEmitter2],
+        inject: [EventEmitter2],
+        name: User.name,
+        useFactory: (eventEmitter: EventEmitter2) => {
+          const schema = UserSchema;
+
+          schema.post('updateOne', (doc: User) => {
+            eventEmitter.emit(UserUpdatedEvent.name, new UserUpdatedEvent(doc._id));
+          });
+
+          return schema;
+        },
+        collection: 'users',
+      },
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService, PenaltyService],
   exports: [UserService],
