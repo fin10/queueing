b26e5b42c7ff162ddbcc7ad1439bc63360ad08b6
fin10 <fender64@gmail.com>
2021-05-04 00:15:47 +0900

    Remove user updated event

--- libs/sdk/src/user/events/user-updated.event.ts
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose';
-
-export class UserUpdatedEvent {
-  constructor(readonly id: mongoose.Types.ObjectId) {}
-}
--- libs/sdk/src/user/user.module.ts
+++ libs/sdk/src/user/user.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
-import { EventEmitter2 } from '@nestjs/event-emitter';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserUpdatedEvent } from './events/user-updated.event';
 import { PenaltyService } from './penalty.service';
 import { User, UserSchema } from './schemas/user.schema';
 import { UserController } from './user.controller';
@@ -9,20 +7,10 @@ import { UserService } from './user.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeatureAsync([
+    MongooseModule.forFeature([
       {
-        imports: [EventEmitter2],
-        inject: [EventEmitter2],
         name: User.name,
-        useFactory: (eventEmitter: EventEmitter2) => {
-          const schema = UserSchema;
-
-          schema.post('updateOne', (doc: User) => {
-            eventEmitter.emit(UserUpdatedEvent.name, new UserUpdatedEvent(doc._id));
-          });
-
-          return schema;
-        },
+        schema: UserSchema,
         collection: 'users',
       },
     ]),
